694314eb5791fd5d8c759f9f1325ce76d3f63119
Madalina Boboc <madaeboboc@gmail.com>
2020-10-25 00:55:53 +0300

    lab00: config cpl

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # cpl
 Compilers Laboratories.
+
+##### Lab0 - Getting started
+Check out the config-cpl.sh.
+
+```
+chmod +x config-cpl.sh
+```
+
+##### Lab1 - Cool exercise
+
---
+++ lab00/config-cpl.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+###############################################################################
+# Script Name	: config-cpl.sh
+# Description	: Configuration script for the Compilers Course
+# Args		: -
+# Author	: Madalina Boboc
+# Email		: madaeboboc@gmail.com
+###############################################################################
+
+# Install Java-14, Spim, some random library
+sudo apt-get install openjdk-14-jdk spim libxkbcommon-x11-0 -y
+
+# Install QtSpim
+curl -L https://sourceforge.net/projects/spimsimulator/files/qtspim_9.1.22_linux64.deb/download --output qtspim_9.1.22_linux64.deb
+sudo dpkg -i qtspim_9.1.22_linux64.deb
+
+# Install Cool interpreter
+curl https://curs.upb.ro/mod/resource/view.php?id=42347 --output cool-interpreters.zip
+unzip cool-interpreters.zip
+cd cool-interpreters.zip
+chmod +x cool
+cd ..
+
+# Install IntelliJ
+sudo snap install intellij-idea-educational --classic
+
+# Install ANTLR4 Plugin Intellij
+echo "Installation complete. Now you need to install the ANTLR4 Plugin in Intellij"
+
+# Set alias for intellij command
+echo "Set alias in ~/.bashrc for intellij-idea-educational to be only intellij. Check out https://github.com/mboboc/scripts-and-fun for example of linux bashrc file."
